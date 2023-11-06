68790277f2f8b2d17819a6eabf1e54e5da50009d
chanuka98 <jeewangacha@gmail.com>
2023-11-06 11:01:10 +0530

    Add Login & Register functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -17,5 +18,46 @@ class UserController extends Controller
             ->get();
 
         return response()->json($results);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $role = Auth::user()->role;
+            return response()->json(['isLogged' => true, 'role' => $role]);
+        } else {
+            return response()->json(['isLogged' => false, 'role' => '']);
+        }
+    }
+
+    public function register(Request $request) {
+        $request -> validate([
+            'name' => 'required',
+            'email' => 'required | email',
+            'password' => 'required | min:6 | alpha_num',
+            'phone' => 'required | min:10 | max:10',
+            'sex' => 'required',
+        ]);
+
+        $user = new User([
+            'name' => $request -> name,
+            'email' => $request -> email,
+            'phone' => $request -> phone,
+            'sex' => $request -> sex,
+            'password' => $request -> password,
+        ]);
+
+        if ($user -> save()) {
+            return response()->json(['message' => 'user created successfully.']));
+        } else {
+            return response()->json(['message'=> 'something went wrong.']);
+        }
     }
 }
